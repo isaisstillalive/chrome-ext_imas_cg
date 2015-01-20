2c59f3e8c09efcc69662001578c361cfe467b5e1
isaisstillalive <isaisstillalive@gmail.com>
2015-01-20 12:44:07 +0900

    直接編成を移動できるように修正

--- deck_edit_top.js
+++ deck_edit_top.js
@@ -14,6 +14,41 @@
         var idols_buttons = $('<div style="max-width:298px; margin:2px 0 6px;" class="displayBox right_float" />');
         $(this).append(idols_buttons);
 
+        // 直接編成を変える
+        $.each([0,1,2,3], function(index, new_pos){
+            if (new_pos == pos) return true;
+            var set_position = $('<div class="grayButton80" style="width:58px; margin:0 6px 0 0;">' + (new_pos+2) + '番手</div>');
+            idols_buttons.append(set_position);
+
+            var count = Math.abs(pos - new_pos);
+            var lift = new Array(count);
+            if (new_pos < pos) {
+                // 上げる
+                for (var i = 0; i < count; i++) {
+                    lift[i] = instanceId;
+                }
+            }
+             else {
+                // 下げるために、一つ下のアイドルを上げることを繰り返す
+                for (var i = 0; i < count; i++) {
+                    lift[i] = instanceIds[pos + 1 + i];
+                }
+            }
+
+            set_position.click(function(){
+                if (is_disabled()) return;
+                var promise = disable_all_buttons();
+
+                $.each(lift, function(index, id){
+                    promise = promise.then(lift_position(id, 0));
+                })
+
+                promise.done(function(){
+                    location.reload();
+                });
+            });
+        });
+
         // リーダーにする
         var set_leader_button = $('<div class="grayButton80" style="width:98px; margin:0;">ﾘｰﾀﾞｰにする</div>');
         idols_buttons.append(set_leader_button);
@@ -48,6 +83,12 @@
         return d.promise();
     }
 
+    function lift_position(id, type)
+    {
+        return function(){
+            return $.get(convertUri('deck/act_priority_up_dec?no=1&s=' + id + '&type=' + type + '&l_frm=Deck_1'));
+        };
+    }
     function remove_unit(id, type)
     {
         return function(){
